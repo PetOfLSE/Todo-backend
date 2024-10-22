3fe1a539aa6d2d215c3dc0263be6756d7fbfcef5
unname <lwer210@naver.com>
2024-10-23 00:33:33 +0900

    UserDetails 구현체

---
+++ src/main/java/com/example/todo/security/custom/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.example.todo.security.custom;
+
+import com.example.todo.user.model.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(new String[]{userEntity.getRole().name()})
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getUserId();
+    }
+}
